09552e7d10fbd90f8d3d1f078867576a0718718b
Marynia <marynia.kolak@gmail.com>
2022-10-22 11:59:21 -0500

    generate theme with JCOIN style

--- access_frontend/src/App.test.tsx
+++ access_frontend/src/App.test.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
---
+++ access_frontend/src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+ typography: {
+   fontFamily: ['"Montserrat"', 'Open Sans'].join(',')
+  }
+})
+
+export default theme;
