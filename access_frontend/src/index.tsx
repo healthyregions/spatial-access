f6676bde49ce418d295fd78142edd0ac8a2393a0
Marynia <marynia.kolak@gmail.com>
2022-10-22 12:15:38 -0500

    Update index.tsx

--- access_frontend/src/index.tsx
+++ access_frontend/src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
