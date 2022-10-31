ccb9e992c7b754afdfcb577624aabfa6470d7b18
Stuart Lynn <stuart.lynn@gmail.com>
2022-10-31 12:19:38 -0400

    splitting content in to pages

---
+++ access_frontend/src/Pages/Methodlogy.tsx
@@ -0,0 +1,35 @@
+import {Box, Typography} from '@mui/material'
+
+
+export default function MethodologyPage(){
+  
+  return (
+    <Box>
+      <Typography variant='h3'>Methodology</Typography>
+      <Typography variant='body1'>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus pretium, diam sit amet gravida condimentum, purus massa efficitur neque, id consequat nisi nibh non orci. Mauris in augue aliquam, accumsan augue at, fringilla turpis. Pellentesque nisi sem, congue sit amet orci id, hendrerit egestas odio. Donec aliquet molestie dolor malesuada porttitor. Cras vehicula justo dolor, non fermentum metus feugiat vel. Nulla faucibus ligula non urna pellentesque, vel tristique augue iaculis. Aenean a leo congue, condimentum quam convallis, accumsan risus. Nulla congue nibh sed mauris ullamcorper volutpat. Vestibulum in libero sit amet tellus scelerisque mattis. Vestibulum maximus nunc felis, nec rutrum neque pellentesque id. Duis accumsan eget mi sed vehicula. Phasellus non vestibulum ligula. Vestibulum vestibulum iaculis nisl a tempor. Aenean semper sagittis massa non placerat. Praesent vel dui tincidunt, ultrices sem tempor, malesuada mi.
+        </Typography>
+
+      <Typography variant='body1'>
+Pellentesque ultricies sem quis nunc rhoncus, id mattis est rhoncus. Donec posuere non nisl a aliquam. Aliquam at eros at sem elementum molestie. Pellentesque varius mi vitae mauris dapibus pulvinar. Nulla in diam bibendum erat porttitor tempus. Morbi tempor ante ac enim euismod placerat. Curabitur semper ipsum vitae bibendum bibendum. Nunc massa dolor, porta nec ultricies at, interdum fermentum dolor. Nullam odio augue, euismod in accumsan sit amet, aliquet id metus. In at risus nunc. Aliquam molestie augue eget aliquam dictum.
+    </Typography>
+
+      <Typography variant='body1'>
+Vivamus vel tortor neque. Suspendisse quis fermentum enim. Nullam commodo, nunc dictum faucibus eleifend, orci enim hendrerit nisl, at varius ante velit sit amet ligula. Aenean sit amet metus nec turpis vehicula bibendum eu vel massa. In hac habitasse platea dictumst. Vestibulum consequat mi eu neque maximus, vel fermentum eros congue. Nulla lectus felis, laoreet eget blandit nec, scelerisque a massa. Cras a posuere dui. In egestas maximus nunc luctus semper. Nunc hendrerit sagittis aliquet. Mauris a efficitur enim.
+        </Typography>
+
+      <Typography variant='body1'>
+Nam non risus id ante auctor consectetur. Donec sodales, ex tempor aliquet vulputate, elit nibh eleifend nunc, condimentum vehicula neque nunc a sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec maximus laoreet velit eu tincidunt. Etiam aliquet arcu sit amet faucibus auctor. Nunc cursus ligula turpis, at auctor ligula tristique mollis. Duis turpis lorem, malesuada quis consequat sed, gravida ut metus. Nunc et aliquam tortor, eu bibendum nulla. Proin augue arcu, commodo id mi sit amet, iaculis malesuada lorem. Nunc non consectetur ante. Sed venenatis urna consectetur, molestie neque sit amet, mollis sapien. Nulla ac felis vulputate, eleifend nunc rutrum, molestie mi. Etiam a diam imperdiet libero suscipit sagittis in vel tortor.
+
+        </Typography>
+
+      <Typography variant='body1'>
+Morbi dictum augue nisi, feugiat mollis enim condimentum in. Etiam vel malesuada lectus, accumsan maximus ex. Maecenas vehicula sem a scelerisque auctor. Nulla eu dolor nisi. Aliquam eu cursus est, vel laoreet nibh. Donec hendrerit felis fermentum urna eleifend aliquam. Sed luctus quam a molestie vulputate.
+
+
+
+      </Typography>
+    </Box>
+  )
+
+}
